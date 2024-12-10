8693857c0978445b72b2fa9355cf6c1f266ce2eb
AkashPaul923 <akashpaul923@gmail.com>
2024-12-10 22:28:33 +0600

    navbar add

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,10 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root/Root";
+import Home from "../Layout/Home";
+import Register from "../Layout/Register";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Root></Root>,
+      errorElement: <h2>Error</h2>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+            path: '/register',
+            element: <Register></Register>
+        },
+      ],
     },
   ]);
 
